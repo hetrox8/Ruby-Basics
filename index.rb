a6125bb279c20254f3c5452af03d1085daf78843
suleiman gacheru <dretrevor8@gmail.com>
2023-09-27 15:37:05 +0300

    add basic codes

---
+++ index.rb
@@ -0,0 +1,23 @@
+2+2
+10/3
+10/3.0
+"Happy" + "coding"
+"Happy" + "\n" +  "coding"
+print "Happy" + "\n" +  "coding" + "\n"
+
+a = 3
+a
+a = "Hello"
+a
+b = "World!"
+puts(a + b)
+
+def hello(name)
+    puts("hello #{name}!")
+end
+hello("suleiman")
+
+def sum(a,b)
+    return a + b
+end
+sum(6,5)
